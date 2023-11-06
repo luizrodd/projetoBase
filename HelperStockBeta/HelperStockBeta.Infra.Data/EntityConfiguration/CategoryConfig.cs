545f84bfe7bf85ce55088c399ff1a374821e3159
Luiz Felipe Rodrigues <98992387+luizrodd@users.noreply.github.com>
2023-11-06 08:10:51 -0300

    previa avaliaçao 23/10 teste unitarios

---
+++ HelperStockBeta/HelperStockBeta.Infra.Data/EntityConfiguration/CategoryConfig.cs
@@ -0,0 +1,25 @@
+﻿using HelperStockBeta.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperStockBeta.Infra.Data.EntityConfiguration
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+            builder.HasData(
+                new Category(1, "Material de Escritorio"),
+                new Category(2, "Tecnologia eletronica"),
+                new Category(3, "Acessorios e Equipamentos")
+                );
+        }
+    }
+}
